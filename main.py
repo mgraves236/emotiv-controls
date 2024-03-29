c8e573f273493af7537518a1e66bea0095d70acd
mgrav <m.graves236@gmail.com>
2024-03-29 11:32:04 +0100

    live sessions

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-import cortex
+import cortex as crtx
+import live_advance as live
+
+# https://medium.com/@kevinjycui/of-mice-and-mind-creating-a-simple-eeg-cursor-control-application-7fcdad703d2f
 
 if __name__ == '__main__':
-    client = cortex.Cortex('', '')
+
+    user = {
+        "license": '',
+        "client_id": '',
+        "client_secret": '',
+        "profile_name": ''
+    }
+    # Create new client
+    # client = crtx.Cortex(user["client_id"], user["client_secret"])
+    # # Run server
+    # client.open()
+    # # Get mental commands
+    # client.get_mental_command_active_action(user["profile_name"])
+    # # Finish working with the script
+    # client.close_session()
+    # client.disconnect_headset()
+    # client.close()
+
+    # Live session
+    l = live.LiveAdvance(user["client_id"], user["client_secret"])
+    l.start(user["profile_name"])
 
